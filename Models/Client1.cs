9573d0b863064d302289ff92c6d8e2c213900043
K G Moota <41505212@mynwu.ac.za>
2024-09-21 14:41:17 +0200

    db scaffold

---
+++ Models/Client1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _41505212__API.Models;
+
+public partial class Client1
+{
+    public Guid ClientId { get; set; }
+
+    public string? ClientName { get; set; }
+
+    public string? PrimaryContactEmail { get; set; }
+
+    public DateTime? DateOnboarded { get; set; }
+}
---
+++ Models/Project1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _41505212__API.Models;
+
+public partial class Project1
+{
+    public Guid ProjectId { get; set; }
+
+    public string? ProjectName { get; set; }
+
+    public string? ProjectDescription { get; set; }
+
+    public DateTime? ProjectCreationDate { get; set; }
+
+    public string? ProjectStatus { get; set; }
+
+    public Guid? ClientId { get; set; }
+}
