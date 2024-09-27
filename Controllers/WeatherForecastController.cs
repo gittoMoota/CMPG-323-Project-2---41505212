cffcccdc974bb2862d90404bfb3e56473e7538c0
K G Moota <41505212@mynwu.ac.za>
2024-09-28 00:20:00 +0200

    “Authorize” attribute inside the “WeatherForecast” controller

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace _41505212__API.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
