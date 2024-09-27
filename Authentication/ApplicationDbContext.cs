ce40d4a6d3ad59d3b532c57edba959f5941d388b
K G Moota <41505212@mynwu.ac.za>
2024-09-28 00:37:54 +0200

    IDesignTimeDbContextFactory

--- Authentication/ApplicationDbContext.cs
+++ Authentication/ApplicationDbContext.cs
@@ -5,23 +5,18 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using _41505212__API.Models;
 
-
-
 namespace JWTAuthentication.Authentication
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
+
         }
-        
-
-
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-          
         }
     }
-
 }
+
---
+++ Authentication/ApplicationDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using JWTAuthentication.Authentication;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace _41505212__API.Authentication
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer("Server=tcp:server-proj2.database.windows.net,1433;Initial Catalog=NWUTechTrends;Persist Security Info=False;User ID=p41505212;Password=project2@41;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
